accc175e22ef1cfd49f0fe8b1ae46e18c120b2aa
Ezharjan <ezharjan@outlook.com>
2021-03-10 11:23:37 +0800

    manually commit

--- gitPusher.lua
+++ gitPusher.lua
@@ -1,7 +1,8 @@
 require('cmdAutoTryer')
 
+local commitInfo = 'auto commit at ostime(' .. string.sub(tostring(os.time()),5) .. ') in comp form LuaScript'
 local addCmd = 'git add .'
-local commitCmd = 'git commit -m \" auto commit in comp form LuaScript\" '
+local commitCmd = 'git commit -m ' .. commitInfo
 local pushCmd =  'git push -u origin master'
 local pullCmd =  'git pull'
 
