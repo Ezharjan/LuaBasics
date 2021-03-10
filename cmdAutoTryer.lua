e7918b5c08e65dacd9b4c72b152db4964616375a
Ezharjan <ezharjan@outlook.com>
2021-03-10 14:22:09 +0800

    --

--- cmdAutoTryer.lua
+++ cmdAutoTryer.lua
@@ -1,15 +1,31 @@
 local function commander(argToExcute)
-    local result = os.execute(argToExcute)
-    return result
+    local result = io.popen(argToExcute)
+    local strInfo = result:read("*all")
+    return strInfo
+end
+
+local function getCrusialInfo(str,crusialStr)
+    if crusialStr ~= nil then
+        local res = string.lower(str).find(str, crusialStr)
+    else
+        local res = string.lower(str).find(str, 'fatal')
+    end
+    return res
 end
 
 function tryTillSucceed(arg,tryTimes)
-    local tryTimes = tryTimes or 1000
+    tryTimes = tryTimes or 1000
     for i = 1,tryTimes,1 do
+        local info = commander(arg)
         print('argument is: ',arg)
-        local res = commander(arg)
-        if re ~= 0 then break end
+        print('info:' .. tostring(info))
+        local res = getCrusialInfo(info)
+        if res ~= '' then
+            print('Conduction succeeded!')
+            break
+        end
     end
+    return
 end
 
 return{
--- osCMDLine.lua
+++ osCMDLine.lua
@@ -1,2 +1,13 @@
-local file = os.execute('git pull')
-print(file) --- 1 or a nonzero number
+-- local res = os.execute('git pull')
+
+local r = io.popen('dir')
+res = r:read('*a')
+-- print('res is ' .. tostring(res)) --- 1 or a nonzero number
+
+if res ~= nil then
+    print('not nil') --- always not nil!
+end
+
+if res ~= '' then
+    print('res is not empty') --- ok, cout when the executio is not successful
+end
