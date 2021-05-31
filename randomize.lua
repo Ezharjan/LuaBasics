88d4b6452b6c17299925b8a6287403974ebdd8f1
Ezharjan <ezharjan@aliyun.com>
2021-05-31 17:43:38 +0800

    auto commit at ostime(454218) in comp form LuaScript

--- randomize.lua
+++ randomize.lua
@@ -24,3 +24,24 @@ print('1: ', math.random(1,100))  --- 第一次执行后获得的总会是相同
 print('1: ', math.random(1,100))  --- 正常获得随机数
 print('1: ', math.random(1,100))
 print('1: ', math.random(1,100))
+
+
+local function getRandomizedNumber(min,max,shouldPrint)
+    local countGreaterThanMin = 0
+    local randomNum = 0
+    math.randomseed(os.time())
+    for i = 1, 100, 1 do
+        randomNum = math.random(min,max)
+        randomNum = math.random(min,max)
+        if randomNum>min then
+            countGreaterThanMin = countGreaterThanMin + 1
+        end
+        if shouldPrint then
+            print(randomNum)
+        end
+    end
+    return randomNum, countGreaterThanMin
+end
+
+local randomNumer, greaterTheOne = getRandomizedNumber(1,2, true)
+print("count of randomized number greater than 1 is :" .. tostring(greaterTheOne))
